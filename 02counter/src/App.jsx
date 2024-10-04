bdc3d597ac898dcbd33af5618976dc685e5e3904
Md Rezaul Hossain <str.rezaul@gmail.com>
2024-10-04 13:19:05 +0600

    day 4

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,11 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  // let counter = 5
+  let [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    // console.log("Clicked", counter);
+    // counter++;
+    setCounter(counter + 1);
+  }
+
+  const reduceValue = () => {
+    setCounter(counter-1);
+  }
 
   return (
     <>
       <h1>Hello world</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={reduceValue}>Remove value</button>
     </>
   )
 }
